9108ac23b67ec46ed71a7b1287b5713847174ba0
jair <jairdeyner730791@gmail.com>
2022-03-27 10:26:23 -0500

    Initial commit

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    this.middlewares();
+
+    // Routes
+    this.routes();
+  }
+
+  middlewares() {
+    // Public Directory
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.send("Hello World!");
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Example app listening on port ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
